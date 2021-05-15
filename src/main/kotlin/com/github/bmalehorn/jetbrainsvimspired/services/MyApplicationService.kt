21f6cc4a84df6a65cc69c369dcfe8c223cd1ad61
GitHub Action <action@github.com>
2021-05-15 20:05:45 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/bmalehorn/jetbrainsvimspired/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.bmalehorn.jetbrainsvimspired.services
+
+import com.github.bmalehorn.jetbrainsvimspired.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
